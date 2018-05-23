04e44083e9250b4a23644bdaa063a6f88d573832
Angelina Filippova <linakirsanova@gmail.com>
2018-05-23 16:51:06 +0300

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -17,4 +17,8 @@ module BlogsHelper
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+
+  def blog_status_color(blog)
+    'color: red;' if blog.published?
+  end
 end
